4094af8d8a693106c2f8c3f84362f7c65f1f86a8
azer86 <azer.ouerimmi86@gmail.com>
2024-11-05 19:41:16 +0000

    First commit

---
+++ data/index.ts
@@ -0,0 +1,247 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  ,
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "pexels-photo-1102797.jfif",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Aboubacar Soumah",
+    description: "FullStack Developers",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/abouabker.jfif",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "EasyPay App",
+    des: "EasyPay est une application Django conçue pour aider les entreprises à gérer efficacement les paies de leurs employés et la gestion des congés. Le système assure une gestion de calcul détaillée, prenant en compte les impôts et les lois fiscales spécifiques à chaque pays.",
+    img: "/easy1.svg",
+    iconLists: [
+      "python-logo-only.png",
+      "django.svg",
+      "bootstrap-logo.svg",
+      "HTML5_logo_and_wordmark.svg.png",
+      ,
+      "mysql.svg",
+    ],
+    link: "https://github.com/Abusooma/payroll-app",
+  },
+  {
+    id: 2,
+    title: "SmartParkingApp",
+    des: "SmartParking is an innovative parking reservation platform developed with Django. It allows customers to easily reserve parking spaces based on their location..",
+    img: "/p2.svg",
+    iconLists: [
+      "python-logo-only.png",
+      "django.svg",
+      "bootstrap-logo.svg",
+      "HTML5_logo_and_wordmark.svg.png",
+      ,
+      "mysql.svg",
+    ],
+    link: "https://github.com/Abusooma/Smart-Parking",
+  },
+  {
+    id: 3,
+    title: "Industrial Production Tracker App",
+    des: "Industrial Production Tracker est une application web développée avec Django pour gérer et suivre la production de cartes magnétiques dans un environnement industriel. L'application permet la saisie, le suivi et l'analyse des données de production, ainsi que la génération de rapports détaillés avec des graphiques..",
+    img: "/p3.svg",
+    iconLists: [
+      "python-logo-only.png",
+      "django.svg",
+      "bootstrap-logo.svg",
+      "HTML5_logo_and_wordmark.svg.png",
+      ,
+      "mysql.svg",
+    ],
+    link: "https://github.com/Abusooma/industrial-production-tracker",
+  },
+  {
+    id: 4,
+    title: "Tunisia Travel Agency App",
+    des: "A comprehensive app designed to provide users with a seamless travel experience across Tunisia",
+    img: "/FreeSample-Vectorizer-io-ki.svg",
+    iconLists: [
+      "python-logo-only.png",
+      "django.svg",
+      "bootstrap-logo.svg",
+      ,
+      "css-3.svg",
+      "mysql.svg",
+    ],
+    link: "https://github.com/Abusooma",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Develop robust backend systems using Python and Django",
+    desc: "Assisted in the development of a web-based platform using Python, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Create RESTful APIs with Django Rest Framework",
+    desc: "Designed and developed RESTful APIs for both web and mobile applications using Django Rest Framework",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Design and manage databases (PostgreSQL, MySQL)",
+    desc: "Designed and managed databases (PostgreSQL, MySQL) for applications from initial concept to deployment.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Enhance responsive  web design with Bootstrap ",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/yourusername", // GitHub link
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://twitter.com/yourusername", // Twitter link
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://linkedin.com/in/yourusername", // LinkedIn link
+  },
+];
